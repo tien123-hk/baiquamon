98c7deacec181f1a830f02874e7c59edd0e415d0
tien123-hk <tiennguyen151003@gmail.com>
2025-04-12 09:34:29 +0700

    commit

--- src/JSX/ShopManagement.jsx
+++ src/JSX/ShopManagement.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../CSS/ShopManagement.css';
+import '../CSS/Shopmanagement.css';
 
 const ShopManagement = () => {
     const [products, setProducts] = useState([]);
